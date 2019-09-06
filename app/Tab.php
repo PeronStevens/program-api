180f49f858aa9dd8fd67280006085cf6016b89ad
peron <pstevens@dkbmed.com>
2019-09-06 17:49:50 -0400

    update

--- app/Tab.php
+++ app/Tab.php
@@ -9,8 +9,4 @@ class Tab extends Model
     public function programs() {
         return $this->belongsToMany(Program::class);
     }
-
-    public function homeContent() {
-        return $this->hasManyThrough('App\Home_Section', 'App\Program_Tab');
-    }
 }
